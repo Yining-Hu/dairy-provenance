09f8d775f0dda26a8f04fe8a2b9d9edfc5a1f1d0
Yining Hu <hulyric@gmail.com>
2021-06-15 16:10:44 +1000

    added tokenPrice

--- migrations/2_deploy_automaticpayment.js
+++ migrations/2_deploy_automaticpayment.js
@@ -3,7 +3,7 @@ const autopayment = artifacts.require('./AutomaticPayment.sol');
 
 module.exports = function(deployer){
     deployer.deploy(farmtoken, 1000000).then(function() {
-        // var tokenPrice = 1000000000000000; // Token price is 0.001 Ether
-        return deployer.deploy(autopayment, farmtoken.address);
+        var tokenPrice = 1; // Token price is 1 Wei
+        return deployer.deploy(autopayment, farmtoken.address, tokenPrice);
     });
 };
