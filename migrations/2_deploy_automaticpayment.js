d5e8c59e43b74b0240237d7fcdaa3b66a88b264e
Yining Hu <hulyric@gmail.com>
2021-06-15 13:13:47 +1000

    combined migration files of automaticpayment and farmtoken

--- migrations/2_deploy_automaticpayment.js
+++ migrations/2_deploy_automaticpayment.js
@@ -1,5 +1,9 @@
+const farmtoken = artifacts.require('./FarmToken.sol');
 const autopayment = artifacts.require('./AutomaticPayment.sol');
 
 module.exports = function(deployer){
-    deployer.deploy(autopayment);
+    deployer.deploy(farmtoken, 1000000).then(function() {
+        // var tokenPrice = 1000000000000000; // Token price is 0.001 Ether
+        return deployer.deploy(autopayment, farmtoken.address);
+    });
 };
--- migrations/4_deploy_farmtoken.js
+++
@@ -1,6 +0,0 @@
-const farmtoken = artifacts.require('./FarmToken.sol');
-
-module.exports = function(deployer){
-    deployer.deploy(farmtoken, 1000000);
-
-};
