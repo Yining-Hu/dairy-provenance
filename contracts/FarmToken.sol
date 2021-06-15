9ce60ac5a37dcd05cb31a635ae2e43dec762d0d7
Yining Hu <hulyric@gmail.com>
2021-06-15 16:11:29 +1000

    removed functions approve, transferFrom

--- contracts/FarmToken.sol
+++ contracts/FarmToken.sol
@@ -4,7 +4,6 @@ contract FarmToken {
     string public name = 'Farm Token';
     string public symbol = 'FMTOKEN';
     string public standard = "Farm Token v1.0";
-    address public admin;
     uint public totalSupply;
 
     event Transfer(
@@ -13,23 +12,11 @@ contract FarmToken {
         uint _value
     );
 
-    event Approval(
-        address indexed _owner,
-        address indexed _spender,
-        uint _value
-    );
-
     mapping(address => uint) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint _initialSupply) public {
-        admin = msg.sender;
-        balanceOf[admin] = _initialSupply;
+        balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
-    }
-
-    function getBalance(address _addr) public view returns(uint) {
-        return balanceOf[_addr];
     }
 
     function transfer(address _to, uint _value) public returns(bool) {
@@ -45,21 +32,8 @@ contract FarmToken {
         return true;
     }
 
-    function approve(address _spender, uint _value) public {
-        allowance[msg.sender][_spender] = _value;
-
-        emit Approval(msg.sender, _spender, _value);
+    function getBalance(address _addr) public view returns(uint) {
+        return balanceOf[_addr];
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public {
-        require(_value <= balanceOf[_from]);
-        require(_value <= allowance[_from][msg.sender]);
-
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
-
-        allowance[_from][msg.sender] -= _value;
-
-        emit Transfer(_from, _to, _value);
-    }
 }
