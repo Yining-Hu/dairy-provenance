053a3d4e6e86374eb92f51218097b5e5150ffcd1
Yining Hu <hulyric@gmail.com>
2021-06-27 21:21:35 +1000

    minor fixes and clean up

--- migrations/2_deploy_automaticpayment.js
+++ migrations/2_deploy_automaticpayment.js
@@ -1,8 +1,9 @@
 const farmtoken = artifacts.require('./FarmToken.sol');
 const autopayment = artifacts.require('./AutomaticPayment.sol');
+const initialSupply = 1000000;
 
 module.exports = function(deployer){
-    deployer.deploy(farmtoken, 1000000).then(function() {
+    deployer.deploy(farmtoken, initialSupply).then(function() {
         var tokenPrice = 1; // Token price is 1 Wei
         return deployer.deploy(autopayment, farmtoken.address, tokenPrice);
     });
